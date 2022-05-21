a7b7daab42f33831bc65aac478e768d85f6cc82c
AlexPaishe <Pashkovskialex@gmail.com>
2022-05-21 19:34:07 +0300

    FixMenu

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -5,37 +5,23 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    protected Text debugText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Transform transform = this.Tran
-        GameObject debugTextObject = transform.Find("DebugText").gameObject;
-        debugText = debugTextObject.GetComponent<Text>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    [SerializeField] private Text _debugText;
 
     public void Play()
     {
         Debug.Log("Play");
-        debugText.text = "Clicked: Play";
+        _debugText.text = "Clicked: Play";
     }
 
     public void OpenSettings()
     {
         Debug.Log("OpenSettings");
-        debugText.text = "Clicked: OpenSettings";
+        _debugText.text = "Clicked: OpenSettings";
     }
 
     public void OpenShop()
     {
         Debug.Log("OpenShop");
-        debugText.text = "Clicked: OpenShop";
+        _debugText.text = "Clicked: OpenShop";
     }
 }
